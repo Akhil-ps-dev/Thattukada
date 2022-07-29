87f05091163869a64a976ab95ed4278e1e1d4dd8
Chatter-Boo <psakhil211@gmail.com>
2022-07-29 10:09:26 +0530

    special menu added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,8 +6,9 @@ import { images } from "../../constants";
 const AboutUs = () => (
   <div
     className="app__aboutus app__bg flex__center section__padding"
-    id="about">
-{/* 
+    id="about"
+  >
+    {/* 
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div> */}
@@ -29,16 +30,13 @@ const AboutUs = () => (
       </div>
     </div>
 
-    
-     <div className="app__aboutus-content_knife flex__center">
+    <div className="app__aboutus-content_knife flex__center">
       <img src={images.knife} alt="aboutknife" />
     </div>
 
-
-
-        <div className="app__aboutus-content flex__center">
+    <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_history">
-        <h2 className="headtext__cormorant">About Us</h2>
+        <h2 className="headtext__cormorant">Lorem Ipsum</h2>
         <img src={images.spoon} alt="" className="spoon__img" />
         <p className="p__opensans">
           Lorem Ipsum has been the industry's standard dummy text ever since the
@@ -51,8 +49,8 @@ const AboutUs = () => (
           Know More
         </button>
       </div>
-    </div> 
-    
+    </div>
+
     {/* <div className="app__aboutus-content_history">
         <h2 className="headtext__cormorant">About Us</h2>
         <img src={images.spoon} alt="" className="spoon__img" />
@@ -65,7 +63,6 @@ const AboutUs = () => (
         </p>
         <button type="button" className="custom__button">Know More</button>
       </div> */}
-
   </div>
 );
 
